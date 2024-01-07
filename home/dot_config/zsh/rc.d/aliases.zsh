170efdf63be9adebd9ade39bb07017c8f6406863
Dan Mooney <maestromoon@proton.me>
2024-01-07 16:08:50 -0700

    Added completions

--- home/dot_config/zsh/rc.d/aliases.zsh
+++ home/dot_config/zsh/rc.d/aliases.zsh
@@ -3,3 +3,11 @@ alias ls='eza --icons=always'
 alias la='eza --icons=always --all'
 alias ll='eza --icons=always --long'
 alias lla='eza --icons=always --long --all'
+
+# Git aliases
+alias gst="git status"
+
+# Chezmoi aliases
+alias chcd="chezmoi cd"
+alias chap="chezmoi apply"
+alias chdf="chezmoi diff"
