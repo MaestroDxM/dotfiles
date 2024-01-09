9791ff0f76c7efd80f13b4d89a05f4f0b310141d
Dan Mooney <maestromoon@proton.me>
2024-01-08 17:53:49 -0700

    Added history config file

---
+++ home/dot_config/zsh/rc.d/history.zsh
@@ -0,0 +1,9 @@
+export HISTFILE="$ZDOTDIR/.zsh_history"
+export HISTSIZE=10000
+export SAVEHIST=10000
+
+setopt HIST_IGNORE_ALL_DUPS # do not populate history with duplicates
+setopt HIST_SAVE_NO_DUPS # do not save duplicate command
+setopt HIST_REDUCE_BLANKS  # remove unnecessary blanks
+setopt INC_APPEND_HISTORY_TIME  # append command to history file immediately after execution
+setopt EXTENDED_HISTORY  # record command start time
